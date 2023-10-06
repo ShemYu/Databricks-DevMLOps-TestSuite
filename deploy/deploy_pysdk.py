8e162a19c9f489c01730cba6ff8f1b9df299672a
ShemYu <shauns4y@gmail.com>
2023-10-06 10:02:11 +0800

    Remove unused

--- deploy/deploy_pysdk.py
+++ deploy/deploy_pysdk.py
@@ -89,35 +89,4 @@ j = w.jobs.create(
   ],
   git_source=git_config
 )
-
 # print(f"View the job at {w.config.host}/#job/{j.job_id}\n")
-
-# # COMMAND ----------
-
-# print("Attempting to create the job. Please wait...\n")
-
-# j = w.jobs.create(
-#   name = job_name,
-#   tasks = [
-#     Task(
-#       description = "Step 1. Jobs description.",
-#       existing_cluster_id = existing_cluster_id, # Runner 
-#       notebook_task = NotebookTask(
-#         base_parameters = {"Test": 123},
-#         notebook_path = notebook_path,
-#         source = GitSource(git_url=git_url, git_provider=provider, git_branch=branch)
-#       ),
-#       task_key = "Step 1."
-#     )
-#   ]
-# )
-
-# print(f"View the job at {w.config.host}/#job/{j.job_id}\n")
-
-# # COMMAND ----------
-
-# # MAGIC %md
-# # MAGIC
-# # MAGIC (*, access_control_list: Optional[List[databricks.sdk.service.iam.AccessControlRequest]]=None, compute: Optional[List[databricks.sdk.service.jobs.JobCompute]]=None, continuous: Optional[databricks.sdk.service.jobs.Continuous]=None, email_notifications: Optional[databricks.sdk.service.jobs.JobEmailNotifications]=None, format: Optional[databricks.sdk.service.jobs.Format]=None, git_source: Optional[databricks.sdk.service.jobs.GitSource]=None, health: Optional[databricks.sdk.service.jobs.JobsHealthRules]=None, job_clusters: Optional[List[databricks.sdk.service.jobs.JobCluster]]=None, max_concurrent_runs: Optional[int]=None, name: Optional[str]=None, notification_settings: Optional[databricks.sdk.service.jobs.JobNotificationSettings]=None, parameters: Optional[List[databricks.sdk.service.jobs.JobParameterDefinition]]=None, queue: Optional[databricks.sdk.service.jobs.QueueSettings]=None, run_as: Optional[databricks.sdk.service.jobs.JobRunAs]=None, schedule: Optional[databricks.sdk.service.jobs.CronSchedule]=None, tags: Optional[Dict[str, str]]=None, tasks: Optional[List[databricks.sdk.service.jobs.Task]]=None, timeout_seconds: Optional[int]=None, trigger: Optional[databricks.sdk.service.jobs.TriggerSettings]=None, webhook_notifications: Optional[databricks.sdk.service.jobs.WebhookNotifications]=None)
-# # MAGIC
-# # MAGIC
